33e886b580f274446b1abe87c60373fe76b9dd46
GrandOichii <i.hawk@yandex.ru>
2022-03-21 15:15:43 +0300

    Changed how data is retrieved, added log, print and cardprint flags

--- edhrec-scraper.go
+++ edhrec-scraper.go
@@ -4,26 +4,82 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 
+	"github.com/GrandOichii/colorwrapper"
 	"github.com/GrandOichii/mtgsdk"
+)
+
+type PrintFlag struct {
+	name  string
+	usage string
+	use   bool
+	print func(cards map[*mtgsdk.Card]int) error
+}
+
+var (
+	printFlags = []*PrintFlag{
+		{
+			name:  "cardprint",
+			usage: "(optional) Prints the cards to the console as cards",
+			use:   false,
+			print: func(cards map[*mtgsdk.Card]int) error {
+				for card, syn := range cards {
+					err := card.CardPrint()
+					if err != nil {
+						return err
+					}
+					fmt.Printf("== Synergy: %d ==\n", syn)
+				}
+				fmt.Println()
+				return nil
+			},
+		},
+		{
+			name:  "print",
+			usage: "(optional) Prints the card names out to the console",
+			use:   false,
+			print: func(cards map[*mtgsdk.Card]int) error {
+				for card, syn := range cards {
+					sign := ""
+					if syn > 0 {
+						sign = "+"
+					}
+					fmt.Printf("\t%s -- %s%d\n", card.Name, sign, syn)
+				}
+				fmt.Println()
+				return nil
+			},
+		},
+	}
 )
 
 var (
 	namesPath     string
 	outPath       string
 	synergyThresh int
+	logF          bool
 )
 
 func init() {
 	flag.StringVar(&namesPath, "src", "", "The path to the file with the commander names")
 	flag.StringVar(&outPath, "out", "", "The out path")
-	flag.IntVar(&synergyThresh, "synergy", 10, "The threshold for the synergy")
+	flag.IntVar(&synergyThresh, "synergy", 10, "(optional) The threshold for the synergy")
+	flag.BoolVar(&logF, "log", false, "(optional) Log the messages")
+
+	for _, pflag := range printFlags {
+		flag.BoolVar(&pflag.use, pflag.name, false, pflag.usage)
+	}
 }
 
 func main() {
 	flag.Parse()
+	if !logF {
+		log.SetOutput(ioutil.Discard)
+	}
 	if namesPath == "" {
 		checkErr(flag.ErrHelp)
 	}
@@ -33,19 +89,23 @@ func main() {
 	// read the commander names
 	cnames, err := readCommanderNames(namesPath)
 	checkErr(err)
-	result := make(map[string]map[string]int, len(cnames))
+	result := make(map[string]map[*mtgsdk.Card]int, len(cnames))
 	// save the cards
 	for _, cname := range cnames {
+		if cname == "" {
+			continue
+		}
 		cards, err := mtgsdk.GetCards(map[string]string{mtgsdk.CardNameKey: cname})
 		checkErr(err)
 		recc, err := cards[0].GetReccomendations(synergyThresh)
 		checkErr(err)
 		result[cname] = recc
 	}
-	data, err := json.MarshalIndent(result, "", "\t")
+	err = writeToFile(result, outPath)
 	checkErr(err)
-	os.WriteFile(outPath, data, 0755)
 	fmt.Println("Done!")
+	err = printCards(result)
+	checkErr(err)
 }
 
 func checkErr(err error) {
@@ -60,4 +120,36 @@ func readCommanderNames(dir string) ([]string, error) {
 		return nil, err
 	}
 	return strings.Split(string(data), "\n"), nil
+}
+
+func writeToFile(m map[string]map[*mtgsdk.Card]int, outPath string) error {
+	result := make(map[string]map[string]int, len(m))
+	for key, cm := range m {
+		result[key] = make(map[string]int, len(cm))
+		for card, syn := range cm {
+			result[key][card.Name] = syn
+		}
+	}
+	data, err := json.MarshalIndent(result, "", "\t")
+	checkErr(err)
+	return os.WriteFile(outPath, data, 0755)
+}
+
+func printCards(cm map[string]map[*mtgsdk.Card]int) error {
+	for _, pflag := range printFlags {
+		if pflag.use {
+			err := colorwrapper.Printf("red-black", "%s:\n", pflag.name)
+			if err != nil {
+				return err
+			}
+			for cname, cmap := range cm {
+				fmt.Printf("Reccomendations for %s:\n", cname)
+				err = pflag.print(cmap)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module github.com/GrandOichii/edhrec-scraper
 
 go 1.17
 
-require github.com/GrandOichii/mtgsdk v1.1.1
+require (
+	github.com/GrandOichii/colorwrapper v0.0.0-20220203103117-b874d1231741
+	github.com/GrandOichii/mtgsdk v1.1.1
+)
 
 require (
 	github.com/GrandOichii/appdata v0.0.0-20220203103444-ee0de59b6cd3 // indirect
 	github.com/GrandOichii/box v0.0.0-20220203103332-865d3af98c3f // indirect
-	github.com/GrandOichii/colorwrapper v0.0.0-20220203103117-b874d1231741 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-rod/rod v0.103.0 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
@@ -17,3 +19,5 @@ require (
 	github.com/ysmood/leakless v0.7.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
+
+replace github.com/GrandOichii/mtgsdk => ../mtgsdk
